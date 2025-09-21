ca9673dd5c4323cfb60fa3e5aeb88a0436122df5
corpsegrinder11 <p.salinas.1194@gmail.com>
2025-09-20 21:31:30 -0300

    chore(lint): Limpiar variable no utilizada en PortfolioGrid

--- src/components/PortfolioGrid.tsx
+++ src/components/PortfolioGrid.tsx
@@ -17,7 +17,7 @@ export default function PortfolioGrid({ proyectos }: Props) {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-      {proyectos.map((proyecto, index) => (
+      {proyectos.map((proyecto) => (
         <PortfolioCard key={proyecto._id} proyecto={proyecto} />
       ))}
     </div>
